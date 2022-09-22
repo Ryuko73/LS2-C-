5ee376ffd8da4fa2c8f73b70b4d47a0fb722f347
Ryuko <mmmmm@mm.ru>
2022-09-22 17:52:32 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Введите номер задачи 1,2,3.");
+int zd = Convert.ToInt32(Console.ReadLine());
+if (zd == 1)
+{
+    Zadacha1();
+}
+if (zd == 2)
+{
+    Zadacha2();
+}
+if (zd == 3)
+{
+    Zadacha3();
+}
+
+void Zadacha1()
+{
+int number = new Random().Next(100, 1000);
+int a = (number / 10);
+int b = (a%10);
+Console.WriteLine(number);
+Console.WriteLine(b);
+}
+
+void Zadacha2()
+{
+int number = new Random().Next(100, 10000);
+int result = number;
+if (number < 100) Console.WriteLine ("нету");
+else
+{
+Convert.ToInt64 (result = (number/10)%10);
+}
+    Console.WriteLine(number);
+    Console.WriteLine(result);
+}
+
+void Zadacha3()
+{
+Console.WriteLine("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Ура,выходной");
+}
+else if (day < 6)
+{
+  Console.WriteLine("Не выходной");
+}
+}
